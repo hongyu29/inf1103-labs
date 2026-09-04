33d91ba0a72832961c897d6f904bcf4dbac0db6f
HongYu <2601702@sit.singaporetech.edu.sg>
2026-09-04 11:36:10 +0800

    First Post

--- INF1103-Labs/hello.py
+++ INF1103-Labs/hello.py
@@ -2,3 +2,11 @@ print("==========================")
 print("Welcome here")
 print("My first post")
 print("==========================")
+
+username = "cool_creator"
+bio = "Fun"
+follower = 100
+
+print("username: ", username)
+print("Bio: ",bio)
+print("Followers: ",follower)
