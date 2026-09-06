59ffeacee5a66b256728c61d28be64f061aa632a
HongYu <2601702@sit.singaporetech.edu.sg>
2026-09-06 14:22:24 +0800

    Added profile variables

--- INF1103-Labs/hello.py
+++ INF1103-Labs/hello.py
@@ -1,12 +1,36 @@
-print("==========================")
-print("Welcome here")
-print("My first post")
-print("==========================")
+# print("==========================")
+# print("Welcome here")
+# print("My first post")
+# print("==========================")
 
-username = "cool_creator"
-bio = "Fun"
-follower = 100
+# username = "cool_creator"
+# bio = "Fun"
+# follower = 100
 
-print("username: ", username)
-print("Bio: ",bio)
-print("Followers: ",follower)
+# print("username: ", username)
+# print("Bio: ",bio)
+# print("Followers: ",follower)
+
+# followers = 100
+
+# followers += 50
+# print("Day 1:", followers)
+
+# followers += 20
+# print("Day 2:", followers)
+
+# followers -= 10
+# print("Day 3:", followers)
+
+username = input("Enter Username: ")
+age = int(input("Enter Age: "))
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("=====================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
+
+if age > 40 and category == "fun":
+    print("You are old what is fun for you??")
