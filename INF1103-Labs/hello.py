340275f8afb1c242de0ea78b452bf7a088fe5977
HongYu <2601702@sit.singaporetech.edu.sg>
2026-09-06 14:57:15 +0800

    dynamic profile

--- INF1103-Labs/hello.py
+++ INF1103-Labs/hello.py
@@ -1,11 +1,9 @@
+username = input("Enter Username: ")
+age = int(input("Enter Age: "))
+category = input("Enter Content Category: ")
 
-followers = 100
-
-followers += 50
-print("Day 1:", followers)
-
-followers += 20
-print("Day 2:", followers)
-
-followers -= 10
-print("Day 3:", followers)
+print("\nInstagram Profile")
+print("=====================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
