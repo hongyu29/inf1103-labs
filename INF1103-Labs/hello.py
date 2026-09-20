30e181c0d62f52a27ae30052d53af96d1bc6d245
HongYu <2601702@sit.singaporetech.edu.sg>
2026-09-20 20:35:02 +0800

    function get valid input

--- INF1103-Labs/hello.py
+++ INF1103-Labs/hello.py
@@ -1,3 +1,16 @@
+# print("==========================")
+# print("Welcome here")
+# print("My first post")
+# print("==========================")
+
+# username = "cool_creator"
+# bio = "Fun"
+# follower = 100
+
+# print("username: ", username)
+# print("Bio: ",bio)
+# print("Followers: ",follower)
+
 username = input("Enter Username: ")
 age = int(input("Enter Age: "))
 category = input("Enter Content Category: ")
